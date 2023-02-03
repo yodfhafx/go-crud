b8280526254b282a512d4077b95e291e4f0fc2ab
yodfhafx <yodfha2014@gmail.com>
2023-02-03 14:34:06 +0700

    setup project

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/", func(ctx *gin.Context) {
+		ctx.String(http.StatusOK, "Welcome project")
+	})
+	r.Run()
+}
