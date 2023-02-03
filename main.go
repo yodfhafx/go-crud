faff8898504439bd910f9599817a026fd8ba8369
yodfhafx <yodfha2014@gmail.com>
2023-02-03 15:18:58 +0700

    group routes api

--- main.go
+++ main.go
@@ -1,15 +1,12 @@
 package main
 
 import (
-	"net/http"
-
 	"github.com/gin-gonic/gin"
+	"github.com/yodfhafx/go-crud/routes"
 )
 
 func main() {
 	r := gin.Default()
-	r.GET("/", func(ctx *gin.Context) {
-		ctx.String(http.StatusOK, "Welcome project")
-	})
+	routes.Serve(r)
 	r.Run()
 }
